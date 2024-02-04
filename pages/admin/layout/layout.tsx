70fc6240a7901944161b2ef813c18b55f7d05f3f
gray <gray.wen@outlook.com>
2024-02-04 17:25:34 +0800

    change menu

--- pages/admin/layout/layout.tsx
+++ pages/admin/layout/layout.tsx
@@ -1,25 +1,31 @@
 import {
   NextUIProvider,
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
   Link,
-  Button,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
+  Listbox,
+  ListboxItem,
+  cn,
 } from '@nextui-org/react';
+import {
+  Icon12Hours,
+  IconActivityHeartbeat,
+  IconBug,
+  IconPuzzle,
+} from '@tabler/icons-react';
 import { useRouter } from 'next/router';
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const [active, setActive] = useState('');
+  const [selectedKeys, setSelectedKeys] = useState();
 
   // 检测路由变化，更新活跃状态
   useEffect(() => {
     setActive(router.pathname);
   }, [router]);
+
+  const activeClass = (pathName: string) => {
+    return pathName === router.pathname ? 'bg-red-100' : '';
+  };
 
   // 创建菜单项组件，用来指示哪个菜单项是活跃的
   const MenuItem = ({ href, children }: any) => (
@@ -35,23 +41,92 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
     </Link>
   );
 
+  const IconWrapper = ({ children, className }: any) => (
+    <div
+      className={cn(
+        className,
+        'flex items-center rounded-small justify-center w-7 h-7'
+      )}
+    >
+      {children}
+    </div>
+  );
+
+  const ItemCounter = ({ number }: any) => (
+    <div className='flex items-center gap-1 text-default-400'>
+      <span className='text-small'>{number}</span>
+      {/* <ChevronRightIcon className='text-xl' /> */}
+    </div>
+  );
+
   return (
     <NextUIProvider>
       <div className='flex h-screen'>
-        <div className='bg-gray-100 w-64 p-6 space-y-6'>
-          <div className='flex items-center space-x-2'>
-            {/* <img src='/logo.png' alt='Logo' className='h-8 w-8' /> */}
-          </div>
-          <nav className='flex flex-col h-auto rounded-medium'>
-            <MenuItem href='/admin/models'>Models</MenuItem>
-            <MenuItem href='/admin/users'>Users</MenuItem>
-            <MenuItem href='/admin/messages'>Messages</MenuItem>
-          </nav>
-          {/* <div className='mt-auto'>
-            <Button color='danger' onClick={() => alert('Logout')}>
-              Logout
-            </Button>
-          </div> */}
+        <div className='bg-white w-64'>
+          <Listbox
+            variant='light'
+            aria-label='User Menu'
+            onAction={(key) => setSelectedKeys(key as any)}
+            className='p-0 gap-0 divide-y divide-default-300/50 dark:divide-default-100/80 bg-content1 w-full h-full overflow-visible shadow-small'
+            itemClasses={{
+              base: 'px-6 rounded-none gap-3 h-12',
+            }}
+            selectionMode='single'
+            selectedKeys={selectedKeys}
+          >
+            <ListboxItem
+              className={`${activeClass('/admin/models')}`}
+              onClick={() => {
+                router.push('/admin/models');
+              }}
+              key='issues'
+              startContent={
+                <IconWrapper className='bg-success/10 text-success'>
+                  <IconBug className='text-lg ' />
+                </IconWrapper>
+              }
+            >
+              Issues
+            </ListboxItem>
+            <ListboxItem
+              className={`${activeClass('/admin/users')}`}
+              onClick={() => {
+                router.push('/admin/users');
+              }}
+              key='pull_requests'
+              startContent={
+                <IconWrapper className='bg-primary/10 text-primary'>
+                  <IconPuzzle className='text-lg ' />
+                </IconWrapper>
+              }
+            >
+              Pull Requests
+            </ListboxItem>
+            <ListboxItem
+              className={`${activeClass('/admin/messages')}`}
+              onClick={() => {
+                router.push('/admin/messages');
+              }}
+              key='discussions'
+              startContent={
+                <IconWrapper className='bg-secondary/10 text-secondary'>
+                  <Icon12Hours className='text-lg ' />
+                </IconWrapper>
+              }
+            >
+              Discussions
+            </ListboxItem>
+            <ListboxItem
+              key='actions'
+              startContent={
+                <IconWrapper className='bg-warning/10 text-warning'>
+                  <IconActivityHeartbeat className='text-lg ' />
+                </IconWrapper>
+              }
+            >
+              Actions
+            </ListboxItem>
+          </Listbox>
         </div>
 
         <div className='flex-grow p-6 overflow-y-auto'>{children}</div>
