1d34c07c6c89906924b3c736281013b5555f48a7
gray <gray.wen@outlook.com>
2024-03-25 15:59:41 +0800

    chenge model folder

--- utils/apis.ts
+++ utils/apis.ts
@@ -2,12 +2,12 @@ import { Model, ModelType } from '@/types/model';
 
 export const getEndpoint = (model: Model) => {
   const Endpoints = {
-    [ModelType.GPT]: 'api/openai',
-    [ModelType.QianFan]: 'api/qianfan',
-    [ModelType.QianWen]: 'api/qianwen',
-    [ModelType.Spark]: 'api/spark',
-    [ModelType.LingYi]: 'api/lingyi',
-    [ModelType.Kimi]: 'api/kimi',
+    [ModelType.GPT]: 'api/models/openai',
+    [ModelType.QianFan]: 'api/models/qianfan',
+    [ModelType.QianWen]: 'api/models/qianwen',
+    [ModelType.Spark]: 'api/models/spark',
+    [ModelType.LingYi]: 'api/models/lingyi',
+    [ModelType.Kimi]: 'api/models/kimi',
   };
   return Endpoints[model.type];
 };
