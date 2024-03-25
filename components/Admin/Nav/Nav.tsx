b6c26905a7fa5f6300eb57aa1fc7069e1cb18d00
gray <gray.wen@outlook.com>
2024-03-25 10:41:40 +0800

    change layout

---
+++ components/Admin/Nav/Nav.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { buttonVariants } from '@/components/ui/button';
+import { useRouter } from 'next/router';
+
+interface NavProps {
+  isCollapsed: boolean;
+  links: {
+    title: string;
+    icon: JSX.Element;
+    url: string;
+  }[];
+}
+
+export function Nav({ links, isCollapsed }: NavProps) {
+  const router = useRouter();
+  const active = (pathName: string) => {
+    return pathName === router.pathname;
+  };
+  return (
+    <div
+      data-collapsed={isCollapsed}
+      className='group flex flex-col gap-4 py-2'
+    >
+      <nav className='grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2'>
+        {links.map((link, index) =>
+          isCollapsed ? (
+            <Tooltip key={index} delayDuration={0}>
+              <TooltipTrigger asChild>
+                <Link
+                  href={link.url}
+                  className={cn(
+                    buttonVariants({
+                      variant: active(link.url) ? 'default' : 'ghost',
+                      size: 'icon',
+                    }),
+                    'h-9 w-9 flex',
+                    active(link.url)
+                      ? 'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white'
+                      : ''
+                  )}
+                >
+                  <span>{link.icon}</span>
+                  <span className='sr-only'>{link.title}</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side='right' className='flex items-center gap-4'>
+                {link.title}
+              </TooltipContent>
+            </Tooltip>
+          ) : (
+            <Link
+              key={index}
+              href={link.url}
+              className={cn(
+                buttonVariants({
+                  variant: active(link.url) ? 'default' : 'ghost',
+                  size: 'sm',
+                }),
+                active(link.url)
+                  ? 'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white'
+                  : '',
+                'justify-start'
+              )}
+            >
+              <span className='mr-2'>{link.icon}</span>
+              {link.title}
+            </Link>
+          )
+        )}
+      </nav>
+    </div>
+  );
+}
--- pages/admin/layout/layout.tsx
+++ pages/admin/layout/layout.tsx
@@ -1,3 +1,11 @@
+import { Nav } from '@/components/Admin/Nav/Nav';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { cn } from '@/lib/utils';
 import {
   IconChartPie,
   IconMessageCircle,
@@ -6,8 +14,10 @@ import {
   IconUserCog,
   IconUsers,
 } from '@tabler/icons-react';
+import { ArchiveX, Inbox, Send, Trash2 } from 'lucide-react';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const AdminLayout = ({
   children,
@@ -18,6 +28,8 @@ const AdminLayout = ({
 }) => {
   const router = useRouter();
   const { t } = useTranslation('admin');
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  let defaultLayout = [265, 1095];
 
   const activeClass = (pathName: string) => {
     return pathName === router.pathname
@@ -25,36 +37,36 @@ const AdminLayout = ({
       : 'text-gray-600';
   };
 
-  const menus = [
+  const links = [
     {
       url: '/admin/dashboard',
       icon: <IconChartPie stroke={1.6} size={22} />,
-      name: t('Dashboard'),
+      title: t('Dashboard'),
     },
     {
       url: '/admin/models',
       icon: <IconSettingsCog stroke={1.6} size={22} />,
-      name: t('Model Configs'),
+      title: t('Model Configs'),
     },
     {
       url: '/admin/users',
       icon: <IconUsers stroke={1.6} size={22} />,
-      name: t('User Management'),
+      title: t('User Management'),
     },
     {
       url: '/admin/user-models',
       icon: <IconUserCog stroke={1.6} size={22} />,
-      name: t('User Models'),
+      title: t('User Models'),
     },
     {
       url: '/admin/messages',
       icon: <IconMessages stroke={1.6} size={22} />,
-      name: t('User Messages'),
+      title: t('User Messages'),
     },
     {
       url: '/',
       icon: <IconMessageCircle stroke={1.6} size={22} />,
-      name: 'Chats',
+      title: 'Chats',
     },
   ];
 
@@ -82,33 +94,80 @@ const AdminLayout = ({
   };
 
   return (
-    <div className={className}>
-      <aside className='fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 shadow-medium border-r'>
-        <div className='h-full px-3 py-4 overflow-y-auto bg-white'>
-          <a
-            onClick={() => {
-              router.push('/admin/dashboard');
-            }}
-            className='flex items-center ps-1 mb-5'
+    <>
+      <TooltipProvider delayDuration={0}>
+        <ResizablePanelGroup
+          direction='horizontal'
+          className='h-full w-full items-stretch'
+        >
+          <ResizablePanel
+            defaultSize={defaultLayout[0]}
+            collapsedSize={10}
+            collapsible={true}
+            minSize={8}
+            maxSize={12}
+            className={cn(
+              isCollapsed && 'transition-all duration-300 ease-in-out',
+              'h-full'
+            )}
           >
-            <img
-              className='h-8 me-3 rounded-sm'
-              alt='chats Logo'
-              src='/chats.png'
-            />
-            <span className='self-center text-md font-medium whitespace-nowrap'>
-              Chats
-            </span>
-          </a>
-          <ul className='space-y-2 font-medium text-[14px]'>
-            {menus.map((m) => {
-              return MenuItem(m);
-            })}
-          </ul>
-        </div>
-      </aside>
-      <div className='p-4 sm:ml-64'>{children}</div>
-    </div>
+            <div className='px-3 py-4 overflow-y-auto'>
+              <a
+                onClick={() => {
+                  router.push('/admin/dashboard');
+                }}
+                className='flex items-center ps-1 mb-1 cursor-pointer'
+              >
+                <img
+                  className='h-8 me-3 rounded-sm'
+                  alt='chats Logo'
+                  src='/chats.png'
+                />
+                <span className='self-center text-md font-medium whitespace-nowrap'>
+                  Chats
+                </span>
+              </a>
+            </div>
+            <Nav isCollapsed={isCollapsed} links={links} />
+          </ResizablePanel>
+          {/* <ResizableHandle withHandle /> */}
+          <ResizablePanel
+            defaultSize={defaultLayout[1]}
+            maxSize={90}
+            minSize={85}
+          >
+            <div className='p-4'>{children}</div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+        {/* <div className={className}>
+        <aside className='fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 shadow-medium border-r'>
+          <div className='h-full px-3 py-4 overflow-y-auto bg-white'>
+            <a
+              onClick={() => {
+                router.push('/admin/dashboard');
+              }}
+              className='flex items-center ps-1 mb-5'
+            >
+              <img
+                className='h-8 me-3 rounded-sm'
+                alt='chats Logo'
+                src='/chats.png'
+              />
+              <span className='self-center text-md font-medium whitespace-nowrap'>
+                Chats
+              </span>
+            </a>
+            <ul className='space-y-2 font-medium text-[14px]'>
+              {menus.map((m) => {
+                return MenuItem(m);
+              })}
+            </ul>
+          </div>
+        </aside>
+        <div className='p-4 sm:ml-64'>{children}</div>
+      </div> */}
+      </TooltipProvider>
+    </>
   );
 };
 
