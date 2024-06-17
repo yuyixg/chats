a6ffa9c98ccbaa54f53791e015bcd5889546b365
gray <gray.wen@outlook.com>
2024-06-17 17:10:25 +0800

    chating disabled search

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,6 +1,8 @@
-import { FC, useRef, useState } from 'react';
+import { FC, useContext, useRef, useState } from 'react';
 
 import { useTranslation } from 'next-i18next';
+
+import { HomeContext } from '@/pages/home/home';
 
 import { IconX } from '@/components/Icons/index';
 
@@ -13,6 +15,9 @@ const Search: FC<Props> = ({ placeholder, searchTerm, onSearch }) => {
   const { t } = useTranslation('sidebar');
   const [query, setQuery] = useState<string>('');
   const timeoutRef = useRef<number | undefined>(undefined);
+  const {
+    state: { messageIsStreaming },
+  } = useContext(HomeContext);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -28,6 +33,7 @@ const Search: FC<Props> = ({ placeholder, searchTerm, onSearch }) => {
   };
 
   const clearSearch = () => {
+    if (messageIsStreaming) return;
     onSearch('');
     setQuery('');
   };
@@ -35,6 +41,7 @@ const Search: FC<Props> = ({ placeholder, searchTerm, onSearch }) => {
   return (
     <div className="relative flex items-center pt-2">
       <input
+        disabled={messageIsStreaming}
         className="w-full flex-1 rounded-md px-3 py-3 pr-10 text-[14px] bg-[#ececec] dark:bg-[#262630] leading-3 border-none outline-none"
         type="text"
         placeholder={t(placeholder) || ''}
