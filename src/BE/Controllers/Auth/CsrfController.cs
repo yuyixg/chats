85a80976f287e996443498650408a13738fe5406
sdcb <sdflysha@qq.com>
2024-07-03 19:25:38 +0800

    add csrftokenservice

---
+++ src/BE/Controllers/Auth/CsrfController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chats.BE.Controllers.Auth;
+
+[Route("api/auth/[controller]")]
+public class CsrfController : ControllerBase
+{
+
+}
---
+++ src/BE/Controllers/Auth/KeycloakController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chats.BE.Controllers.Auth;
+
+[Route("api/auth/signin/[controller]")]
+public class KeycloakController : ControllerBase
+{
+}
