b296dbdfe06b53e55a38ff43b89705ffe02aeb4b
sdcb <sdflysha@qq.com>
2024-07-08 21:54:53 +0800

    Prompts CRUD.

---
+++ src/BE/Controllers/Chats/Prompts/Dtos/PromptType.cs
@@ -0,0 +1,7 @@
+﻿namespace Chats.BE.Controllers.Chats.Prompts.Dtos;
+
+public enum PromptType
+{
+    Public = 1,
+    Private = 2,
+}
---
+++ src/BE/Controllers/Chats/Prompts/Dtos/PromptsDto.cs
@@ -0,0 +1,31 @@
+﻿using Chats.BE.DB;
+using System.Text.Json.Serialization;
+
+namespace Chats.BE.Controllers.Chats.Prompts.Dtos;
+
+public record PromptsDto
+{
+    [JsonPropertyName("id")]
+    public required Guid Id { get; init; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; init; }
+
+    [JsonPropertyName("content")]
+    public required string? Content { get; init; }
+
+    [JsonPropertyName("description")]
+    public required string? Description { get; init; }
+
+    public Prompt ToPrompt(Guid createUserId) => new()
+    {
+        Id = Id,
+        Name = Name,
+        Content = Content,
+        Description = Description,
+        CreatedAt = DateTime.UtcNow,
+        CreateUserId = createUserId,
+        Type = (int)PromptType.Private,
+        UpdatedAt = DateTime.UtcNow
+    };
+}
