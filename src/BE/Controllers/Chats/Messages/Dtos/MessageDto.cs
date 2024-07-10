696d9ab860b66b1bf949eb3320fd9ac6cd191bec
sdcb <sdflysha@qq.com>
2024-07-10 19:17:32 +0800

    road to get messages

---
+++ src/BE/Controllers/Chats/Messages/Dtos/MessageDto.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Chats.BE.Controllers.Chats.Messages.Dtos;
+
+public record MessageDto
+{
+    [JsonPropertyName("id")]
+    public required string Id { get; init; }
+
+    [JsonPropertyName("parentId")]
+    public required string? ParentId { get; init; }
+
+    [JsonPropertyName("role")]
+    public required string Role { get; init; }
+
+    [JsonPropertyName("content")]
+    public required MessageContentDto Content { get; init; }
+
+    [JsonPropertyName("createdAt")]
+    public required DateTime CreatedAt { get; init; }
+}
+
+public record UserMessageDto : MessageDto;
+
+public record AssistantMessageDto : MessageDto
+{
+    [JsonPropertyName("inputTokens")]
+    public required int InputTokens { get; init; }
+
+    [JsonPropertyName("outputTokens")]
+    public required int OutputTokens { get; init; }
+
+    [JsonPropertyName("inputPrice")]
+    public required string InputPrice { get; init; }
+
+    [JsonPropertyName("outputPrice")]
+    public required string OutputPrice { get; init; }
+
+    [JsonPropertyName("duration")]
+    public required int Duration { get; init; }
+
+    [JsonPropertyName("modelName")]
+    public required string? ModelName { get; init; }
+}
+
+public record MessageContentDto
+{
+    [JsonPropertyName("text")]
+    public required string Text { get; init; }
+
+    [JsonPropertyName("image")]
+    public required List<string> Image { get; init; }
+}
---
+++ src/BE/Controllers/Chats/Messages/MessagesController.cs
@@ -0,0 +1,16 @@
+﻿using Chats.BE.Controllers.Chats.Messages.Dtos;
+using Chats.BE.DB;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chats.BE.Controllers.Chats.Messages;
+
+[Route("api/messages"), Authorize]
+public class MessagesController(ChatsDB db) : ControllerBase
+{
+    //[HttpGet]
+    //public async Task<ActionResult<MessageDto[]>> GetMessages([FromQuery] Guid chatId)
+    //{
+    //    throw new NotImplementedException();
+    //}
+}
