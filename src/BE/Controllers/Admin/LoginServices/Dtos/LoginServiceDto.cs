f83ae7769e846a2da9dca67b8624eea6dcfcb696
sdcb <sdflysha@qq.com>
2024-08-30 19:17:15 +0800

    add login service

---
+++ src/BE/Controllers/Admin/LoginServices/Dtos/LoginServiceDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Chats.BE.Controllers.Admin.LoginServices.Dtos;
+
+public record LoginServiceDto
+{
+    [JsonPropertyName("id")]
+    public required Guid Id { get; init; }
+
+    [JsonPropertyName("type")]
+    public required string Type { get; init; }
+
+    [JsonPropertyName("configs")]
+    public object? Configs { get; init; }
+
+    [JsonPropertyName("enabled")]
+    public required bool Enabled { get; init; }
+
+    [JsonPropertyName("createdAt")]
+    public required DateTime CreatedAt { get; init; }
+}
---
+++ src/BE/Controllers/Admin/LoginServices/LoginServiceController.cs
@@ -0,0 +1,14 @@
+﻿using Chats.BE.Controllers.Admin.Common;
+using Chats.BE.Controllers.Admin.LoginServices.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chats.BE.Controllers.Admin.LoginServices;
+
+[Route("api/admin/login-service2"), AuthorizeAdmin]
+public class LoginServiceController : ControllerBase
+{
+    public async Task<ActionResult<LoginServiceDto>> GetLoginServices(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
