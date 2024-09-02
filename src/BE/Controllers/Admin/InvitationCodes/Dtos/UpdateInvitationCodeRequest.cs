812a89a2988e263c7e7bbf1379827e59dafe8cb8
sdcb <sdflysha@qq.com>
2024-09-02 12:51:34 +0800

    complete invitation code

---
+++ src/BE/Controllers/Admin/InvitationCodes/Dtos/UpdateInvitationCodeRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Chats.BE.Controllers.Admin.InvitationCodes.Dtos;
+
+public record UpdateInvitationCodeRequest
+{
+    [JsonPropertyName("id")]
+    public required Guid Id { get; init; }
+
+    [JsonPropertyName("count")]
+    public required short Count { get; init; }
+}
+
+public record CreateInvitationCodeRequest
+{
+    [JsonPropertyName("value")]
+    public required string Name { get; init; }
+
+    [JsonPropertyName("count")]
+    public required short Count { get; init; }
+}
--- src/BE/Controllers/Admin/InvitationCodes/InvitationCodeController.cs
+++ src/BE/Controllers/Admin/InvitationCodes/InvitationCodeController.cs
@@ -1,6 +1,8 @@
 ﻿using Chats.BE.Controllers.Admin.Common;
 using Chats.BE.Controllers.Admin.InvitationCodes.Dtos;
+using Chats.BE.Controllers.Common;
 using Chats.BE.DB;
+using Chats.BE.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +18,8 @@ public class InvitationCodeController(ChatsDB db) : ControllerBase
             .Select(x => new InvitationCodeDto()
             {
                 Id = x.Id,
-                Value = x.Value, 
-                Count = x.Count, 
+                Value = x.Value,
+                Count = x.Count,
                 Username = db.Users
                     .Where(u => u.Id == x.CreateUserId)
                     .Select(x => x.Username)
@@ -25,5 +27,59 @@ public class InvitationCodeController(ChatsDB db) : ControllerBase
             })
             .ToArrayAsync(cancellationToken);
         return Ok(data);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateInvitationCode([FromBody] UpdateInvitationCodeRequest req, CancellationToken cancellationToken)
+    {
+        InvitationCode? code = await db.InvitationCodes.FindAsync([req.Id], cancellationToken);
+        if (code == null)
+        {
+            return NotFound();
+        }
+
+        code.Count = req.Count;
+        if (db.ChangeTracker.HasChanges())
+        {
+            await db.SaveChangesAsync();
+        }
+        return NoContent();
+    }
+
+    [HttpDelete("{invitationCodeId}")]
+    public async Task<ActionResult> DeleteInvitationCode(Guid invitationCodeId, CancellationToken cancellationToken)
+    {
+        InvitationCode? code = await db.InvitationCodes.FindAsync([invitationCodeId], cancellationToken);
+        if (code == null)
+        {
+            return NotFound();
+        }
+
+        if (await db.UserInvitations.AnyAsync(x => x.InvitationCodeId == invitationCodeId, cancellationToken))
+        {
+            code.IsDeleted = true;
+        }
+        else
+        {
+            db.InvitationCodes.Remove(code);
+        }
+
+        await db.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpPost]
+    public async Task CreateInvitationCode([FromBody] CreateInvitationCodeRequest req, [FromServices] CurrentUser currentUser, CancellationToken cancellationToken)
+    {
+        db.InvitationCodes.Add(new InvitationCode()
+        {
+            Id = Guid.NewGuid(), 
+            Value = req.Name,
+            Count = req.Count,
+            CreatedAt = DateTime.UtcNow,
+            CreateUserId = currentUser.Id, 
+            IsDeleted = false, 
+        });
+        await db.SaveChangesAsync(cancellationToken);
     }
 }
