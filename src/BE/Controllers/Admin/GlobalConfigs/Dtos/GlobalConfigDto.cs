8d51d0c6de6daf6dc357028316be62ea43b450c7
sdcb <sdflysha@qq.com>
2024-09-02 12:45:22 +0800

    add global config controller

---
+++ src/BE/Controllers/Admin/GlobalConfigs/Dtos/GlobalConfigDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Chats.BE.Controllers.Admin.GlobalConfigs.Dtos;
+
+public record GlobalConfigDto
+{
+    [JsonPropertyName("key")]
+    public required string Key { get; init; }
+
+    [JsonPropertyName("value")]
+    public required string Value { get; init; }
+
+    [JsonPropertyName("description")]
+    public required string? Description { get; init; }
+}
---
+++ src/BE/Controllers/Admin/GlobalConfigs/GlobalConfigController.cs
@@ -0,0 +1,55 @@
+﻿using Chats.BE.Controllers.Admin.Common;
+using Chats.BE.Controllers.Admin.GlobalConfigs.Dtos;
+using Chats.BE.Controllers.Common;
+using Chats.BE.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace Chats.BE.Controllers.Admin.GlobalConfigs;
+
+[Route("api/admin/global-configs"), AuthorizeAdmin]
+public class GlobalConfigController(ChatsDB db) : ControllerBase
+{
+    [HttpGet]
+    public async Task<GlobalConfigDto[]> GetGlobalConfigs(CancellationToken cancellationToken)
+    {
+        GlobalConfigDto[] data = await db.Configs
+            .Select(x => new GlobalConfigDto()
+            {
+                Key = x.Key,
+                Value = x.Value,
+                Description = x.Description,
+            })
+            .ToArrayAsync(cancellationToken);
+        return data;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateGlobalConfig([FromBody] GlobalConfigDto req, CancellationToken cancellationToken)
+    {
+        Config? config = await db.Configs.FindAsync([req.Key], cancellationToken);
+        if (config == null)
+        {
+            return NotFound();
+        }
+
+        // ensure value is valid json
+        try
+        {
+            JsonDocument.Parse(req.Value);
+        }
+        catch (JsonException)
+        {
+            return this.BadRequestMessage("Invalid JSON");
+        }
+
+        config.Value = req.Value;
+        config.Description = req.Description;
+        if (db.ChangeTracker.HasChanges())
+        {
+            await db.SaveChangesAsync(cancellationToken);
+        }
+        return NoContent();
+    }
+}
