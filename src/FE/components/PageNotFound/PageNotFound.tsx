41012adff4cba8734c2bd3ff27ffc0dd7d2b230a
gray <gray.wen@outlook.com>
2024-09-02 17:14:34 +0800

    fixed share chat page style issues

--- src/FE/components/PageNotFound/PageNotFound.tsx
+++ src/FE/components/PageNotFound/PageNotFound.tsx
@@ -4,13 +4,13 @@ import { Button } from '../ui/button';
 
 const PageNotFound = () => {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <main className="grid min-h-full place-items-center dark:bg-[#262630] bg-white py-24 sm:py-32">
       <div className="text-center">
         <p className="text-3xl font-semibold text-primary">404</p>
-        <h1 className="mt-4 text-lg font-bold tracking-tight text-gray-900">
+        <h1 className="mt-4 text-lg font-bold tracking-tight text-gray-900 dark:text-gray-100">
           抱歉，我们找不到您要找的页面。
         </h1>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-10 flex items-center justify-center">
           <Link href="/">
             <Button className="h-8 w-32">返回首页</Button>
           </Link>
