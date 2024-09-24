5261ca2e33eb46b51bda7e22c43d086d68bcb065
sdcb <sdflysha@qq.com>
2024-09-24 19:06:47 +0800

    fix error

--- src/BE/Controllers/Chats/Messages/Dtos/MessageDto.cs
+++ src/BE/Controllers/Chats/Messages/Dtos/MessageDto.cs
@@ -104,7 +104,7 @@ public record MessageContentResponse
         return new MessageContentResponse()
         {
             Text = string.Join("\n", groups[DBMessageContentType.Text].Select(Encoding.Unicode.GetString)),
-            Image = groups[DBMessageContentType.ImageUrl].Select(Encoding.UTF8.GetString).ToList() switch { [] => null, var x => x }
+            Image = groups[DBMessageContentType.ImageUrl].Select(Encoding.UTF8.GetString).ToList() switch { [] => null, var x => x },
             Error = string.Join("\n", groups[DBMessageContentType.Error].Select(Encoding.UTF8.GetString)) switch { "" => null, var x => x }
         };
     }
