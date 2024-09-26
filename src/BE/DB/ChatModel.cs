d1911c106fef121bf7e6d30e79f301d24723631b
sdcb <sdflysha@qq.com>
2024-09-26 18:52:44 +0800

    ChatModel columns to upper case

--- src/BE/DB/ChatModel.cs
+++ src/BE/DB/ChatModel.cs
@@ -12,53 +12,39 @@ namespace Chats.BE.DB;
 public partial class ChatModel
 {
     [Key]
-    [Column("id")]
     public Guid Id { get; set; }
 
-    [Column("modelProvider")]
     [StringLength(1000)]
     public string ModelProvider { get; set; } = null!;
 
-    [Column("modelVersion")]
     [StringLength(1000)]
     public string ModelVersion { get; set; } = null!;
 
-    [Column("name")]
     [StringLength(1000)]
     public string Name { get; set; } = null!;
 
-    [Column("rank")]
     public int? Rank { get; set; }
 
-    [Column("remarks")]
     [StringLength(1000)]
     public string? Remarks { get; set; }
 
-    [Column("modelKeysId")]
     public Guid ModelKeysId { get; set; }
 
-    [Column("fileServiceId")]
     public Guid? FileServiceId { get; set; }
 
-    [Column("fileConfig")]
     [StringLength(2048)]
     public string? FileConfig { get; set; }
 
-    [Column("modelConfig")]
     [StringLength(2048)]
     public string ModelConfig { get; set; } = null!;
 
-    [Column("priceConfig")]
     [StringLength(2048)]
     public string PriceConfig { get; set; } = null!;
 
-    [Column("enabled")]
     public bool Enabled { get; set; }
 
-    [Column("createdAt")]
     public DateTime CreatedAt { get; set; }
 
-    [Column("updatedAt")]
     public DateTime UpdatedAt { get; set; }
 
     [InverseProperty("ChatModel")]
