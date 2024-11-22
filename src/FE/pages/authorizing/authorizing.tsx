a2f3a4a76f79f73ae68ea16387aedc13db47dcdd
sdcb <sdflysha@qq.com>
2024-11-22 18:50:58 +0800

    fix keycloak

--- src/FE/pages/authorizing/authorizing.tsx
+++ src/FE/pages/authorizing/authorizing.tsx
@@ -12,8 +12,15 @@ export default function Authorizing() {
   const { t } = useTranslation();
   const [isClient, setIsClient] = useState(false);
   const router = useRouter();
-  const { code, provider } = router.query as { code: string; provider: string };
+  const [everStarted, setEverStarted] = useState(false);
+
   useEffect(() => {
+    const { code, provider } = router.query as { code: string; provider: string };
+    if (!router.isReady || everStarted) {
+      return;
+    }
+    
+    setEverStarted(true);
     setIsClient(true);
     if (!code) {
       router.push('/login');
@@ -33,7 +40,7 @@ export default function Authorizing() {
       .catch(() => {
         toast.error(t('Authorization failed. Please try again later.'));
       });
-  }, []);
+  }, [router.isReady]);
   return (
     <>
       {isClient && (
