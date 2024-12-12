e2355ba89f9812635fcb84a847cb02a14c35fd19
gray <gray.wen@outlook.com>
2024-12-12 16:15:39 +0800

    add bg image/ change icon

--- src/FE/pages/login/index.tsx
+++ src/FE/pages/login/index.tsx
@@ -125,8 +125,8 @@ export default function LoginPage() {
       {!loading && isClient && (
         <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
           <div className="relative hidden h-full flex-col  p-10 text-white dark:text-black lg:flex dark:border-r">
-            <div className="absolute inset-0 dark:bg-gray-50 bg-zinc-900" />
-            <div className="relative z-20 flex items-center text-lg font-medium">
+            <div className="absolute inset-0 dark:bg-gray-50 bg-zinc-900 bg-[url('/bg.webp')] bg-cover" />
+            <div className="relative z-20 flex items-center text-lg font-medium text-white">
               <img
                 src="/icons/logo.png"
                 width={32}
