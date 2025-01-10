a4effc1bb5113b28f1a8b76c06fd5132adac4e03
sdcb <sdflysha@qq.com>
2025-01-10 23:15:10 +0800

    road to google ai

---
+++ src/BE/Services/ChatServices/Implementations/OpenAI/GoogleAIChatService.cs
@@ -0,0 +1,22 @@
+﻿using Chats.BE.DB;
+using OpenAI.Chat;
+using System.Runtime.CompilerServices;
+
+namespace Chats.BE.Services.ChatServices.Implementations.OpenAI;
+
+public class GoogleAIChatService(Model model) : OpenAIChatService(model, new Uri("https://generativelanguage.googleapis.com/v1beta/openai/"))
+{
+    protected override Dtos.ChatTokenUsage GetUsage(ChatTokenUsage usage)
+    {
+        IDictionary<string, BinaryData> b = GetSerializedAdditionalRawData(usage)!;
+        return new Dtos.ChatTokenUsage
+        {
+            InputTokens = b["promptTokens"].ToObjectFromJson<int>(),
+            OutputTokens = b["completionTokens"].ToObjectFromJson<int>(),
+            ReasoningTokens = 0,
+        };
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_SerializedAdditionalRawData")]
+    private extern static IDictionary<string, BinaryData>? GetSerializedAdditionalRawData(ChatTokenUsage usage);
+}
---
+++ src/scripts/db-migration/2025/20250110-googleai.sql
@@ -0,0 +1,3 @@
+INSERT INTO [ModelProvider]([Id], [Name], [InitialHost], [InitialSecret])
+VALUES (13, 'Google AI', NULL, '');
+
