340d6869d87dbce2b9ec4a9845f0eea816ea0c42
sdcb <sdflysha@qq.com>
2025-01-16 23:18:15 +0800

    initial support of version check

---
+++ src/BE/Controllers/Admin/GlobalConfigs/Dtos/CheckUpdateResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Chats.BE.Controllers.Admin.GlobalConfigs.Dtos;
+
+public record CheckUpdateResponse
+{
+    [JsonPropertyName("hasNewVersion")]
+    public required bool HasNewVersion { get; init; }
+
+    [JsonPropertyName("tagName")]
+    public required string TagName { get; init; }
+
+    [JsonPropertyName("currentVersion")]
+    public required int CurrentVersion { get; init; }
+}
---
+++ src/BE/Controllers/Admin/GlobalConfigs/VersionController.cs
@@ -0,0 +1,30 @@
+﻿using Chats.BE.Controllers.Admin.Common;
+using Chats.BE.Controllers.Admin.GlobalConfigs.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chats.BE.Controllers.Admin.GlobalConfigs;
+
+[AuthorizeAdmin, Route("api/version")]
+public class VersionController : ControllerBase
+{
+    const int buildVersion = 0;
+
+    [HttpGet("current")]
+    public ActionResult GetCurrentVersion()
+    {
+        return Ok(buildVersion);
+    }
+
+    [HttpPost("check-update")]
+    public async Task<ActionResult> CheckUpdate(CancellationToken cancellationToken)
+    {
+        string tagName = await GitHubReleaseChecker.SdcbChats.GetLatestReleaseTagNameAsync(cancellationToken);
+        bool hasNewVersion = GitHubReleaseChecker.IsNewVersionAvailableAsync(tagName, buildVersion);
+        return Ok(new CheckUpdateResponse
+        {
+            CurrentVersion = buildVersion,
+            HasNewVersion = hasNewVersion,
+            TagName = tagName,
+        });
+    }
+}
