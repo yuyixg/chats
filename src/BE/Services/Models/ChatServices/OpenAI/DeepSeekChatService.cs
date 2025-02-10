44a542d2cc4d150230633f3e2a15b064405a3d60
sdcb <sdflysha@qq.com>
2025-02-10 18:39:33 +0800

    deepseek now accept temperature

--- src/BE/Services/Models/ChatServices/OpenAI/DeepSeekChatService.cs
+++ src/BE/Services/Models/ChatServices/OpenAI/DeepSeekChatService.cs
@@ -8,11 +8,6 @@ public class DeepSeekChatService(Model model) : OpenAIChatService(model, new Uri
 {
     protected override Task<ChatMessage[]> FEPreprocess(IReadOnlyList<ChatMessage> messages, ChatCompletionOptions options, ChatExtraDetails feOptions, CancellationToken cancellationToken)
     {
-        if (Model.ModelReference.Name == "deepseek-reasoner")
-        {
-            // deepseek-reasoner model does not support temperature
-            options.Temperature = null;
-        }
         options.SetMaxTokens(Model.ModelReference.MaxResponseTokens); // https://api-docs.deepseek.com/zh-cn/quick_start/pricing default 4096 but max 8192
         return base.FEPreprocess(messages, options, feOptions, cancellationToken);
     }
