11762025b0e9d3d36ce191b82c75f15409e4d58a
sdcb <sdflysha@qq.com>
2025-02-10 22:52:51 +0800

    rollback because no use

--- src/FE/next.config.js
+++ src/FE/next.config.js
@@ -2,34 +2,11 @@ const isDev = process.env?.NODE_ENV === 'development';
 console.log('NODE_ENV', process.env?.NODE_ENV);
 console.log('-------------------');
 
-const runtimeCaching = [
-  {
-    urlPattern: /^https?.*\/api\/.*$/i,
-    handler: 'NetworkOnly',
-    options: {
-      cacheName: 'api',
-      networkTimeoutSeconds: 15,
-    },
-  },
-  {
-    urlPattern: /^https?.*\.(png|jpg|jpeg|svg|gif|webp|avif)$/i,
-    handler: 'CacheFirst',
-    options: {
-      cacheName: 'images',
-      expiration: {
-        maxEntries: 100,
-        maxAgeSeconds: 86400, // 1 天
-      },
-    },
-  },
-];
-
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: !isDev,
   skipWaiting: !isDev,
   disable: isDev,
-  runtimeCaching,
 });
 
 /** @type {import('next').NextConfig} */
