fc5382eddcc69d2efef5d7351ad8c93cbd3cf94f
sdcb <sdflysha@qq.com>
2025-02-12 13:12:50 +0800

    fix yi issue

---
+++ src/scripts/db-migration/2025/20250212-yi.sql
@@ -0,0 +1,71 @@
+-- Region Parameters
+DECLARE @p0 SmallInt = 301
+DECLARE @p1 Date = '2024-01-01'
+-- EndRegion
+UPDATE [ModelReference]
+SET [PublishDate] = @p1
+WHERE [Id] = @p0
+GO
+
+-- Region Parameters
+DECLARE @p0 SmallInt = 302
+DECLARE @p1 Date = '2024-01-01'
+-- EndRegion
+UPDATE [ModelReference]
+SET [PublishDate] = @p1
+WHERE [Id] = @p0
+GO
+
+-- Region Parameters
+DECLARE @p0 SmallInt = 304
+DECLARE @p1 Date = '2024-01-01'
+-- EndRegion
+UPDATE [ModelReference]
+SET [PublishDate] = @p1
+WHERE [Id] = @p0
+GO
+
+-- Region Parameters
+DECLARE @p0 SmallInt = 305
+DECLARE @p1 Date = '2024-01-01'
+-- EndRegion
+UPDATE [ModelReference]
+SET [PublishDate] = @p1
+WHERE [Id] = @p0
+GO
+
+-- Region Parameters
+DECLARE @p0 SmallInt = 306
+DECLARE @p1 Date = '2024-01-01'
+-- EndRegion
+UPDATE [ModelReference]
+SET [PublishDate] = @p1
+WHERE [Id] = @p0
+GO
+
+-- Region Parameters
+DECLARE @p0 SmallInt = 307
+DECLARE @p1 Date = '2024-01-01'
+-- EndRegion
+UPDATE [ModelReference]
+SET [PublishDate] = @p1
+WHERE [Id] = @p0
+GO
+
+-- Region Parameters
+DECLARE @p0 SmallInt = 308
+DECLARE @p1 Date = '2024-01-01'
+-- EndRegion
+UPDATE [ModelReference]
+SET [PublishDate] = @p1
+WHERE [Id] = @p0
+GO
+
+-- Region Parameters
+DECLARE @p0 SmallInt = 303
+DECLARE @p1 NVarChar(1000) = 'yi-vision-v2'
+-- EndRegion
+UPDATE [ModelReference]
+SET [Name] = @p1
+WHERE [Id] = @p0
+GO
